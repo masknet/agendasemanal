2a8204256ecbfc057273e87b21af144390c83c8c
Oseas Salim <69397780+masknet@users.noreply.github.com>
2023-04-09 21:29:05 -0300

    commit inicial
    O primeiro commit do site

---
+++ firebase.js
@@ -0,0 +1,13 @@
+    var firebaseConfig = {
+		apiKey: "AIzaSyBkrBLKuklZgPm1nz2G997ULiYycZMb9F8",
+      authDomain: "avisoseeventos.firebaseapp.com",
+      databaseURL: "https://avisoseeventos-default-rtdb.firebaseio.com",
+      projectId: "avisoseeventos",
+      storageBucket: "avisoseeventos.appspot.com",
+      messagingSenderId: "247706769451",
+      appId: "1:247706769451:web:ce31cd9d0ca22cd267b26e",
+      measurementId: "G-QE1Z4RQ60T"
+	};
+	// Inicialização do Firebase
+	firebase.initializeApp(firebaseConfig);
+	var database = firebase.database();
